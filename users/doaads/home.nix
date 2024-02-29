78665510be6ded157fc24124d6c4335ceacd0d79
doaads <nowobart@gmail.com>
2024-02-29 18:21:20 +0100

    changes

--- users/doaads/home.nix
+++ users/doaads/home.nix
@@ -10,7 +10,7 @@
 
   home.packages = with pkgs; [
 	# Shell
-	zsh 
+	zsh gnome.gnome-terminal
 
 	# Browser
 	firefox
@@ -48,6 +48,10 @@
     # '')
   ];
 
+  programs.gnome-terminal = {
+	enable = true;
+  }
+
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
@@ -59,6 +63,4 @@
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
-
- 
 }
