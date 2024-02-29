77ce0f060ddc833631fe3b1b3a4d6a150d4c8b5d
doaads <nowobart@gmail.com>
2024-02-29 18:21:39 +0100

    changes

--- users/doaads/home.nix
+++ users/doaads/home.nix
@@ -50,7 +50,7 @@
 
   programs.gnome-terminal = {
 	enable = true;
-  }
+  };
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
